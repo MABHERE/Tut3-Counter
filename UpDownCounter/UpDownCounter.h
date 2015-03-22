8d0a2d7a344378f94998ad4a73785e727a58aabe
mabhere <213571329@stu.ukzn.ac.za>
2015-03-22 03:48:48 +0200

    upDownCoounter cpp fill in progress

--- UpDownCounter/UpDownCounter.h
+++ UpDownCounter/UpDownCounter.h
@@ -8,14 +8,15 @@ private:
 public:
 	UpDownCounter(int str, int end, int stp);// constructor
 	~UpDownCounter();// deconstructor
-	void setStart();
-	void setStop();
+	void setStart(int str);
+	void setStop(int end);
 	int getStart();
 	int getStop();
-	int decrement();
-	int increment();
-	UpDownCounter& operator++();//prefix increment
+	void decrement();
+	void increment();
+	void display();
+/*	UpDownCounter& operator++();//prefix increment
 	UpDownCounter& operator--();//prefix decrement
 	UpDownCounter& operator++(int);//postfix increment
-	UpDownCounter& operator--(int);//postfix increment
+	UpDownCounter& operator--(int);//postfix increment*/
 };
